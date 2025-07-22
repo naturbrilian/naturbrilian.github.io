cacf4429000426651873eeabbb6d6c75d9cf1d1d
naturbrilian <gingalibadeidara@gmail.com>
2025-07-22 04:49:25 +0000

    Deploying to gh-pages from @ naturbrilian/site@878a3e49afeeb27fda9c7cff4f09e254cc6f5614 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -806,6 +806,17 @@ ninja.data = [{
           window.location.href = "/site/blog/2021/its-been-a-long-time-since-updating-this-blog/";
         
       },
+    },{id: "post-kustomisasi-tema-untuk-browser",
+      
+        title: "Kustomisasi tema untuk browser",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/site/blog/2021/artikel-spesial-kustomisasi-tema-untuk/";
+        
+      },
     },{id: "post-novel-the-failure-alchemist-of-the-royal-capital-my-job-is-a-failure-so-i-ll-manage-my-store-carefreely-bahasa-indonesia-rena-translations",
       
         title: '[Novel] The Failure Alchemist of the Royal Capital ~My job is a failure... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
